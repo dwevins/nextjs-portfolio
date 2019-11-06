8542c9d945b9ca8e0285e898e1cb3e0dcf328a2d
Woody Evins <woodyevins@Woodys-MacBook-Pro.local>
2019-11-05 20:01:27 -0600

    fpo page layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
 import App from 'next/app'
 import React from 'react'
+import styled from 'styled-components'
 import { ThemeProvider } from 'styled-components'
 import { Resets, Theme } from '../global'
+import { Nav } from '../components'
+
+const NavPlaceholder = styled.div`
+  flex-shrink: 0;
+  min-height: 100vh;
+  width: ${p => p.theme.sizing.navWidth};
+`
+
+const PageWrapper = styled.div`
+  display: flex;
+  min-height: 100vh;
+  width: 100vw;
+`
 
 export default class MyApp extends App {
   render() {
@@ -9,7 +23,11 @@ export default class MyApp extends App {
     return (
       <ThemeProvider theme={Theme}>
         <Resets />
-        <Component {...pageProps} />
+        <PageWrapper>
+          <Nav />
+          <NavPlaceholder />
+          <Component {...pageProps} />
+        </PageWrapper>
       </ThemeProvider>
     )
   }
