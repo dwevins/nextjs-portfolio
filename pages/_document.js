9f80d328a684893f940d161abfb2e600d300514c
Woody Evins <woodyevins@Woodys-MacBook-Pro.local>
2019-11-05 18:58:48 -0600

    move favicon to _document

--- pages/_document.js
+++ pages/_document.js
@@ -26,6 +26,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* Step 5: Output the styles in the head  */}
           {this.props.styleTags}
+          <link rel="icon" href="/favicon.ico" />
         </Head>
         <body>
           <Main />
